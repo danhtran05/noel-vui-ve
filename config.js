2852d0ac711cbd58cb74926dad6e954285ccee1a
duongduong <93088754+duongduong1106@users.noreply.github.com>
2021-12-25 00:04:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const giftUrl = 'http://nodemy.vn';
 const eventName = 'Noel vui vẻ';
 const titleCard = 'Người con gái anh yêu';
 const contentCard = 'Người ta nói bệnh nào cũng có thuốc chữa. Anh đã không tin điều đó nữa đến khi anh gặp em. Anh đã phát hiện ra anh đã mang một căn bệnh mà có lẽ,Có lẽ sẽ không bao giờ có thuốc điều trị. Đó là căn bệnh hạnh phúc nhất mà anh mai mắn được mắc phải. Đó là bệnh yêu em quá nhiều.Yêu em cho đến khi nào anh không còn tồn tại. Hãy cứ tin em nhé. Chúc em noel vui vẽ. ';
-const giftImconst age = 'MyAvatar.png';
+const giftImconst age = 'vy.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
